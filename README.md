232c3c5f8c9a2d6c00d13518a1a386a2c35e31d9
Chad Duffin <chadduffin01@gmail.com>
2017-11-28 12:18:09 -0800

    Added basic comments to code.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tiny Matrix
 
 This class allows for the creation of a matrix whose data type is 4 bits. It uses a contiguous chunk of memory and bitmasks to retrieve the desired index.
+
+This code is intentionally condensed.
--- tiny_matrix.cpp
+++ tiny_matrix.cpp
@@ -7,6 +7,8 @@ TinyMatrix::TinyMatrix(unsigned int p_row, unsigned int p_col) {
   col = p_col;
   llen = p_row*p_col;
 
+  // divides by two
+  // adds one if odd
   len = (llen >> 1)+(llen & 0x1);
 
   matrix = new unsigned char[len];
@@ -23,6 +25,8 @@ void TinyMatrix::Set(unsigned int p_row, unsigned int p_col, unsigned int p_val)
     unsigned int i = p_col+p_row*col,
                  j = i >> 1;
 
+    // determines if the target cell is in the upper or lower half of the byte
+    // sets and shifts, or simply sets the value depending on if it was upper or lower
     *(matrix+j) = (i & 0x1) ? ((*(matrix+j) & 0xF) | ((p_val << 4) & 0xF0)) : ((*(matrix+j) & 0xF0) | (p_val & 0xF));
   }
 }
@@ -32,6 +36,8 @@ unsigned int TinyMatrix::Get(unsigned int p_row, unsigned int p_col) {
     unsigned int i = p_col+p_row*col,
                  j = i >> 1;
 
+    // determines the target cell and extracts the data
+    // shifts if it is the upper half of the byte
     return (i & 0x1) ? ((*(matrix+j) & 0xF0) >> 4) : (*(matrix+j) & 0xF);
   }
 
